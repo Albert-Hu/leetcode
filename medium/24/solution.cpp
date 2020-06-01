529dc8a4900066f8ff254aae5f15108f5ca171cb
Albert <0x.albert@gmail.com>
2020-06-01 15:28:22 +0800

    solved problem 24.

---
+++ medium/24/solution.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+#ifdef LOCAL_TEST
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+#endif
+
+class Solution {
+public:
+  ListNode* swapPairs(ListNode* head) {
+    if (head == nullptr) {
+      return head;
+    }
+    if (head->next == nullptr) {
+      return head;
+    }
+
+    ListNode *next = head->next;
+    ListNode *swapped = swapPairs(next->next);
+
+    next->next = head;
+    head->next = swapped;
+
+    return next;
+  }
+};
+
+#ifdef LOCAL_TEST
+
+void dump(ListNode* head) {
+  if (head != nullptr) {
+    cout << head->val << (head->next == nullptr ? "" : "->");
+    dump(head->next);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  ListNode *head;
+
+  head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+  dump(s.swapPairs(head));
+  cout << endl;
+
+  head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+  dump(s.swapPairs(head));
+  cout << endl;
+
+  return 0;
+}
+
+#endif
