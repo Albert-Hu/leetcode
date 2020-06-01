ee8b674d3eb5fef3ebe4de4671378b0eb117535d
Albert <0x.albert@gmail.com>
2020-06-01 16:09:37 +0800

    solved problem 61.

--- medium/24/solution.cpp
+++ medium/24/solution.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 #ifdef LOCAL_TEST
 
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 #endif
 
 class Solution {
