93ca8a65c58af92371d99fb6110e2b398d22e449
Albert <0x.albert@gmail.com>
2020-06-22 16:37:56 +0800

    solved problem 162.

---
+++ solutions/medium/162/solution.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findPeakElement(vector<int>& nums) {
+    int left = 0, right = nums.size() - 1;
+    
+    while (left < right) {
+      int peek = left + (right - left) / 2;
+      if (nums[peek] < nums[peek+1]) left = peek + 1;
+      else right = peek;
+    }
+
+    return left;
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  vector<int> nums;
+
+  nums.push_back(1);
+  nums.push_back(2);
+  nums.push_back(3);
+  nums.push_back(1);
+  cout << s.findPeakElement(nums) << endl;
+
+  nums.clear();
+  nums.push_back(1);
+  nums.push_back(2);
+  nums.push_back(1);
+  nums.push_back(3);
+  nums.push_back(5);
+  nums.push_back(6);
+  nums.push_back(4);
+  cout << s.findPeakElement(nums) << endl;
+
+  return 0;
+}
+
+#endif
