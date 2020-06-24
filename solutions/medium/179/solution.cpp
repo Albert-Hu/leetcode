a73893787db63d0aa947cb8f7d61a1add5e07bcb
Albert <0x.albert@gmail.com>
2020-06-24 17:32:11 +0800

    solved problem 179.

---
+++ solutions/medium/179/solution.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+  string largestNumber(vector<int>& nums) {
+    vector<string> intStrings = toStringArray(nums);
+
+    sort(intStrings.begin(), intStrings.end(), [](string a, string b) {
+      return (b+a) < (a+b);
+    });
+
+    return merge(intStrings);
+  }
+
+  string merge(vector<string> &intStrings) {
+    string str;
+    if (intStrings.size() > 0) {
+      str = intStrings[0];
+    }
+    for (int index = 1; index < intStrings.size(); index++) {
+      if (str.length() == 1 && str[0] == '0') break;
+      str += intStrings[index];
+    }
+    return str;
+  }
+
+  vector<string> toStringArray(vector<int> &nums) {
+    vector<string> intStrings;
+    for (auto n : nums) {
+      intStrings.push_back(to_string(n));
+    }
+    return intStrings;
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  vector<int> nums;
+
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  nums.push_back(0);
+  cout << s.largestNumber(nums) << endl;
+
+  nums.clear();
+  nums.push_back(10);
+  nums.push_back(2);
+  cout << s.largestNumber(nums) << endl;
+
+  nums.clear();
+  nums.push_back(3);
+  nums.push_back(30);
+  nums.push_back(34);
+  nums.push_back(5);
+  nums.push_back(9);
+  cout << s.largestNumber(nums) << endl;
+
+  return 0;
+}
+
+#endif
