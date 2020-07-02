3ab1d14e1f825907b30b1913029ccd7067041c21
Albert <0x.albert@gmail.com>
2020-07-02 18:24:40 +0800

    solved problem 69.

---
+++ solutions/easy/69/solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  int mySqrt(int x) {
+    int left = 1;
+    int right = x;
+
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+
+    while (left < right) {
+      int middle = left + (right - left) / 2;
+      if ((x / middle) >= middle) {
+        left = middle + 1;
+      } else {
+        right = middle;
+      }
+    }
+
+    return right - 1;
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  
+  cout << s.mySqrt(4) << endl;
+  cout << s.mySqrt(8) << endl;
+
+  return 0;
+}
+
+#endif
