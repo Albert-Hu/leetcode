7487f8f05c8514dfe2de7b388c9842c7b0a45560
Albert <0x.albert@gmail.com>
2020-07-08 16:54:46 +0800

    solved problem 300.

---
+++ solutions/medium/300/solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int lengthOfLIS(vector<int>& nums) {
+    vector<int> record;
+
+    if (nums.size() > 0) {
+      record.push_back(nums[0]);
+    }
+    for (int i = 1; i < nums.size(); i++) {
+      int n = nums[i];
+      if (n > record.back()) {
+        record.push_back(n);
+      } else {
+        *lower_bound(record.begin(), record.end(), n) = n;
+      }
+    }
+
+    return record.size();
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  vector<int> nums{10,9,2,5,3,7,101,18};
+
+  cout << s.lengthOfLIS(nums) << endl;
+
+  return 0;
+}
+
+#endif
