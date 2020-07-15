cfb784e8caba747f3f39e91022a85f3de3f4eb1d
Albert <0x.albert@gmail.com>
2020-07-15 14:10:59 +0800

    solved problem 191.

---
+++ solutions/easy/191/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  int hammingWeight(uint32_t n) {
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+      if ((n >> i) & 1) count++;
+    }
+    return count;
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+
+  cout << s.hammingWeight(11) << endl;
+  cout << s.hammingWeight(128) << endl;
+
+  return 0;
+}
+
+#endif
