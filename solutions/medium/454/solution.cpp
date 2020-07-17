6f9d0093b806a7f6ec139d3a0dc6882788bb74b5
Albert <0x.albert@gmail.com>
2020-07-17 11:07:02 +0800

    solved problem 454.

---
+++ solutions/medium/454/solution.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+  int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+    const int n = A.size();
+    int count = 0;
+    map<long long int, int> twoSum;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        twoSum[A[i] + B[j]]++;
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        long long int sum = -(C[i] + D[j]);
+        if (twoSum.count(sum) > 0)
+          count += twoSum[sum];
+      }
+    }
+
+    return count;
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  vector<int> A{1, 2};
+  vector<int> B{-2, -1};
+  vector<int> C{-1, 2};
+  vector<int> D{0, 2};
+
+  cout << s.fourSumCount(A, B, C, D) << endl;
+
+  return 0;
+}
+
+#endif
