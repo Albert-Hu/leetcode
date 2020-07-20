713d695f7e420efd48744c139c7f2c8418acb88d
Albert <0x.albert@gmail.com>
2020-07-20 10:05:15 +0800

    solved problem 242.

---
+++ solutions/easy/242/solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  bool isAnagram(string s, string t) {
+    vector<int> count(26, 0);
+
+    for (auto c : s) {
+      int index = static_cast<int>(c - 'a');
+      count[index]++;
+    }
+
+    for (auto c : t) {
+      int index = static_cast<int>(c - 'a');
+      count[index]--;
+    }
+
+    for (auto c : count) if (c != 0) return false;
+
+    return true;
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+
+  cout << (s.isAnagram("anagram", "nagaram") ? "true" : "false") << endl;
+  cout << (s.isAnagram("rat", "car") ? "true" : "false") << endl;
+
+  return 0;
+}
+
+#endif
