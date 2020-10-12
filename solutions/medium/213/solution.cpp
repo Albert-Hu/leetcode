2144d0ff01fb9c43f1943238fc4d89ac44ae6f5e
Albert <0x.albert@gmail.com>
2020-10-12 14:01:11 +0800

    solved problem 213.

---
+++ solutions/medium/213/solution.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int rob(vector<int> &nums, int start, int end) {
+    int a = 0, b = 0;
+    while (start < end) {
+      int c = max(nums[start] + a, b);
+      a = b;
+      b = c;
+      start++;
+    }
+    return b;
+  }
+
+  int rob(vector<int>& nums) {
+    if (nums.size() == 0)
+      return 0;
+    if (nums.size() == 1)
+      return nums.front();
+    return max(rob(nums, 0, nums.size() - 1), rob(nums, 1, nums.size()));
+  }
+};
+
+#ifdef LOCAL_TEST
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  vector<vector<int>> nums = {
+    {2, 3, 2},
+    {1, 2, 3, 1},
+    {0}
+  };
+
+  for (auto n : nums)
+    cout << s.rob(n) << endl;
+
+  return 0;
+}
+
+#endif
