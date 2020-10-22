7e55b7ee01197d9066e975ba04aa580129e8b577
Albert <0x.albert@gmail.com>
2020-10-22 23:08:19 +0800

    updated the python script.

--- create_solution.py
+++ create_solution.py
@@ -3,13 +3,17 @@ from distutils.dir_util import copy_tree
 
 import argparse, os
 
-def copy(dest):
+def copy(number):
+  upper_bound = ((number + 49) / 50) * 50
+  group = '{}-{}'.format(upper_bound - 49, upper_bound)
+  dest = os.path.join('solutions', group, str(number))
   if os.path.exists(dest):
     print('The solution already exists.')
   else:
     copy_tree('template', dest)
+    print('created a new solution at {}', dest)
 
 if __name__ == '__main__':
   args = argparse.ArgumentParser()
-  args.add_argument('path', help='Path to solution.', type=str)
-  copy(args.parse_args().path)
+  args.add_argument('number', help='The number of problem.', type=int)
+  copy(args.parse_args().number)
