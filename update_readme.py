56ab01cf57f3288f02baa322c6ab1ce61678d1fa
Albert <0x.albert@gmail.com>
2020-10-22 22:36:52 +0800

    updated the update_readme.py.

--- update_readme.py
+++ update_readme.py
@@ -2,13 +2,10 @@ from __future__ import print_function
 
 import os
 
-cnt = 0
-
 def update_readme(path, content):
   with open(path, 'w') as readme:
     readme.write(content)
   print('updated {}.'.format(path))
-  cnt += 1
 
 root = 'solutions'
 groups = os.listdir(root)
@@ -22,9 +19,15 @@ for group in groups:
   problems = os.listdir(group_path)
   problems.sort(key=lambda x: int(x))
   for problem in problems:
-    with open(os.path.join(group_path, problem, 'README.md'), 'r') as readme:
+    problem_path = os.path.join(group_path, problem)
+    with open(os.path.join(problem_path, 'README.md'), 'r') as readme:
       title = ' '.join(readme.readline().split()[1:])
-    content += '- {}, [{}]({}/{}/README.md)\n'.format(problem, title, group, problem)
+    incomplete = '(incomplete)'
+    solution_md = os.path.join(problem_path, 'Solution.md')
+    if os.path.exists(solution_md):
+      if os.path.getsize(solution_md) > 0:
+        incomplete = ''
+    content += '- {}, [{}]({}/{}/README.md){}\n'.format(problem, title, group, problem, incomplete)
   content += '\n'
 update_readme(os.path.join(root, 'README.md'), content)
 
@@ -36,10 +39,14 @@ for group in groups:
   problems = os.listdir(os.path.join(group_path))
   problems.sort(key=lambda x: int(x))
   for problem in problems:
-    with open(os.path.join(group_path, problem, 'README.md'), 'r') as readme:
+    problem_path = os.path.join(group_path, problem)
+    with open(os.path.join(problem_path, 'README.md'), 'r') as readme:
       title = ' '.join(readme.readline().split()[1:])
-    content += '{}, [{}]({}/README.md)\n'.format(problem, title, problem)
+    incomplete = '(incomplete)'
+    solution_md = os.path.join(problem_path, 'Solution.md')
+    if os.path.exists(solution_md):
+      if os.path.getsize(solution_md) > 0:
+        incomplete = ''
+    content += '{}, [{}]({}/README.md){}\n'.format(problem, title, problem, incomplete)
   content += '\n'
   update_readme(readme_path, content)
-
-print('{} files are updated.'.format(cnt))
