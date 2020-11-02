428bfa3179961a1611bed5789a52a1245310ab37
Albert <0x.albert@gmail.com>
2020-11-02 22:18:22 +0800

    update the bubble_sort.cpp.

--- algorithm/sort/bubble_sort.cpp
+++ algorithm/sort/bubble_sort.cpp
@@ -1,4 +1,11 @@
 #include "sort.h"
 
 void sort(vector<int> &array) {
+  for (int length = array.size(); length > 0; length--) {
+    for (int i = 1; i < length; i++) {
+      if (array[i] < array[i - 1]) {
+        swap(array[i], array[i - 1]);
+      }
+    }
+  }
 }
