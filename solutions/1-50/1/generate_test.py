41fd3830f636a9bbd6f8af2ab9be3f9ddfee4466
Albert <0x.albert@gmail.com>
2020-11-14 00:22:19 +0800

    added test data.

---
+++ solutions/1-50/1/generate_test.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+
+import random
+
+'''
+The inputs are repeating by:
+1. Target
+2. Numbers
+3. Answer
+'''
+
+# Test 1
+print('0')
+print('0 0')
+print('0 1')
+
+# Test 2
+print('0')
+print('1 -1')
+print('0 1')
+
+# Test 3
+print('-999')
+print('1 -1000')
+print('0 1')
+
+# Test 4
+print('5')
+print('1 2 3')
+print('1 2')
+
+# Test 5
+print('4')
+print('1 2 3')
+print('0 2')
+
+# Test 6
+print('3')
+print('1 2 3')
+print('0 1')
+
+# Remaining Test
+
+
