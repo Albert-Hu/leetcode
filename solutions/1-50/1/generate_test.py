83e2eb896bd6a41f6573d2ec65582d5c4f5300df
Albert <0x.albert@gmail.com>
2020-11-15 22:51:36 +0800

    Added some test cases.

--- solutions/1-50/1/generate_test.py
+++ solutions/1-50/1/generate_test.py
@@ -9,6 +9,27 @@ The inputs are repeating by:
 3. Answer
 '''
 
+def dump_test(test):
+  print(str(test[0]))
+  print(' '.join([str(n) for n in test[1]]))
+  print('{} {}'.format(test[2][0], test[2][1]))
+
+def generate_test(start=-1e9, end=1e9, amount=100):
+  array = [random.randrange(start, end) for _ in range(amount)]
+  indices = list(range(amount))
+  indices.sort(key=lambda i: array[i])
+  if random.choice([True, False]):
+    answer = [indices[-1], indices[-2]]
+    array[indices[-1]] = array[indices[-1]] + 2
+    array[indices[-2]] = array[indices[-2]] + 1
+    target = array[indices[-1]] + array[indices[-2]]
+  else:
+    answer = [indices[0], indices[1]]
+    array[indices[0]] = array[indices[0]] - 2
+    array[indices[1]] = array[indices[1]] - 1
+    target = array[indices[0]] + array[indices[1]]
+  return target, array, answer
+
 # Test 1
 print('0')
 print('0 0')
@@ -39,6 +60,14 @@ print('3')
 print('1 2 3')
 print('0 1')
 
-# Remaining Test
+# Test 7
+dump_test(generate_test(-10, 10, 10))
 
+# Test 8
+dump_test(generate_test(-100, 100, 100))
 
+# Test 9
+dump_test(generate_test(-1000, 1000, 1000))
+
+# Test 10
+dump_test(generate_test(int(-1e8), int(1e8), int(1e5)))
--- solutions/1-50/1/main.cpp
+++ solutions/1-50/1/main.cpp
@@ -1,11 +1,74 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <vector>
 
 #include "solution.h"
 
 using namespace std;
 
+bool get_answer(vector<int> &answer) {
+  int n;
+  string line;
+  getline(cin, line);
+  istringstream iss(line);
+
+  while (iss >> n) {
+    answer.push_back(n);
+  }
+
+  return answer.size() == 2;
+}
+
+bool get_array(vector<int> &array) {
+  int n;
+  string line;
+  getline(cin, line);
+  istringstream iss(line);
+
+  while (iss >> n) {
+    array.push_back(n);
+  }
+
+  return array.size() >= 2;
+}
+
+bool get_target(int &target) {
+  string line;
+  getline(cin, line);
+  istringstream iss(line);
+
+  return (iss >> target) ? true : false;
+}
+
 int main(int argc, char *argv[]) {
+  int test_case = 1;
+  while (1) {
+    int target;
+    vector<int> array;
+    vector<int> answer;
+
+    if (!get_target(target)) break;
+    if (!get_array(array)) break;
+    if (!get_answer(answer)) break;
+
+    Solution solution;
+    vector<int> result = solution.twoSum(array, target);
+
+    cout << "Test " << std::left << setw(3) << test_case++ << ": ";
+    if (result.size() != 2) {
+      cout << "[FAIL]";
+    } else {
+      if (max(result[0], result[1]) == max(answer[0], answer[1]) && min(result[0], result[1]) == min(answer[0], answer[1])) {
+        cout << "[PASS]";
+      } else {
+        cout << "[FAIL]";
+      }
+    }
+    cout << endl;
+
+  }
+#if 0
   /*
     Format of Test Case:
     {{test}, {answer}}
@@ -36,6 +99,6 @@ int main(int argc, char *argv[]) {
     }
     cout << endl;
   }
-
+#endif
   return 0;
 }
