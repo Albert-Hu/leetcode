77886a4cfdff65c05588c1a614a4a5c7ab2b602c
Albert <0x.albert@gmail.com>
2020-11-27 10:48:59 +0800

    Updated the template.

--- template/solution.cpp
+++ template/solution.cpp
@@ -1,11 +1,7 @@
-#include <iostream>
-
 #ifdef LOCAL_TEST
 #include "solution.h"
 #else
 // put the class definition here
 #endif
-
-using namespace std;
 
 // implement the solution here.
--- template/solution.h
+++ template/solution.h
@@ -1,6 +1,10 @@
 #ifndef _SOLUTION_H_
 #define _SOLUTION_H_
 
+#include <iostream>
+
+using namespace std;
+
 // put the class definition here
 
 #endif /* _SOLUTION_H_ */
