30057fde57f00ddaeff9d1e2c2e0ef692cce9d4c
Albert <0x.albert@gmail.com>
2020-11-27 10:53:26 +0800

    Updated the main.cpp.

--- template/main.cpp
+++ template/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <iomanip>
 
 #include "solution.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  // TODO: add test cases and verification.
+  int test_case = 1;
+
+  while (1) {
+    // TODO: Implement the rest...
+    cout << "Test " << std::left << setw(3) << test_case++ << ": ";
+    if (0) {
+      cout << "[PASS]";
+    } else {
+      cout << "[FAIL]";
+    }
+    cout << endl;
+  }
+
   return 0;
 }
